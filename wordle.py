5b71276c1d57fb4cdfa80e9a55707e6b3af7d2ab
FIRCALiviu <117108317+FIRCALiviu@users.noreply.github.com>
2022-11-07 09:10:02 +0200

    Add files via upload

--- wordle.py
+++ wordle.py
@@ -9,23 +9,32 @@ def choose_word():
 
 
 def get_user_input(chosen):
-    word = input("Word = ")
+    word = input("Word = ").upper()
+    
+    if word not in data:
+        if(word =="QUIT"):
+            print("thanks for playing")
+            return
+        print("Not in word list")
+        get_user_input(chosen)
+        return
     res = matches(word, chosen)
-
     if "1" in res or "2" in res:
-        print("Wrong! Try again.")
         print(res)
         get_user_input(chosen)
         return
 
     print("Got it!")
-    play_again = input("Wanna play again? Respond with y for yes and n for no: ")
+    play_again = input("Want play again? Respond with y for yes and n for no: ")
     if play_again == "y":
         chosen = choose_word()
         get_user_input(chosen)
     else:
         print("Thank you for playing!")
 
+print("welcome to wordle, 1 means gray (the letter isn't in the word you should guess)")
+print("2 means yellow (the letter is in the word you should guess but not at the right position and 3 means yellow but it is at the right position)")
+print("enter \"quit\" to stop the gui")
 
 chosen = choose_word()
 get_user_input(chosen)
